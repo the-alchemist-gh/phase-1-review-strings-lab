3e4b47ddef914b0696f37ba59bb2e628829ea8bb
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-04-25 14:45:01 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}`;
+
+const shortGreeting = `${welcomeMessage.slice(0,7)}, ${currentUser.slice(0,1)}${welcomeMessage.slice(welcomeMessage.length-1)}`;
+
+console.log(shortGreeting);
